cbde0d7865c58e35d1c99303305bcafcdd320beb
Adam Rasburn <BlackForestCheeseCake@protonmail.ch>
2021-08-01 09:02:05 +1200

    fixed too many error messages for ensure_export

--- RHutil/JetsonNano_gpio.cpp
+++ RHutil/JetsonNano_gpio.cpp
@@ -1,9 +1,9 @@
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 // #include <errno.h>
-#include <unistd.h>
 #include <fcntl.h>
+#include <unistd.h>
 // #include <poll.h>
 #include <string.h>
 
@@ -15,32 +15,32 @@
 #define POLL_TIMEOUT (3 * 1000) /* 3 seconds */
 #define MAX_BUF 64
 
+bool silence_errors = false;
+
 /****************************************************************
  * gpio_export
  ****************************************************************/
 int gpio_export(unsigned int gpio)
 {
-	int fd, len, err;
-	char buf[MAX_BUF];
+  int fd, len, err;
+  char buf[MAX_BUF];
 
-	fd = open(SYSFS_GPIO_DIR "/export", O_WRONLY);
-	if (fd < 0)
-	{
-		perror("gpio/export");
-		return fd;
-	}
-	// printf("gpio_export(%u)\n", gpio);
+  fd = open(SYSFS_GPIO_DIR "/export", O_WRONLY);
+  if (fd < 0) {
+    perror("gpio/export");
+    return fd;
+  }
+  // printf("gpio_export(%u)\n", gpio);
 
-	len = snprintf(buf, sizeof(buf), "%d", gpio);
-	err = write(fd, buf, len);
-	close(fd);
+  len = snprintf(buf, sizeof(buf), "%d", gpio);
+  err = write(fd, buf, len);
+  close(fd);
 
-	if (err < 0)
-	{
-		perror("gpio_export write failed : Pin probably already exported ");
-	}
+  if (err < 0 && !silence_errors) {
+    perror("gpio_export write failed : Pin probably already exported ");
+  }
 
-	return err < 0 ? err : 0;
+  return err < 0 ? err : 0;
 }
 
 /****************************************************************
@@ -48,21 +48,21 @@ int gpio_export(unsigned int gpio)
  ****************************************************************/
 int gpio_unexport(unsigned int gpio)
 {
-	int fd, len, err;
-	char buf[MAX_BUF];
+  int fd, len, err;
+  char buf[MAX_BUF];
 
-	fd = open(SYSFS_GPIO_DIR "/unexport", O_WRONLY);
-	if (fd < 0)
-	{
-		perror("gpio/unexport");
-		return fd;
-	}
-	// printf("gpio_unexport(%u)\n", gpio);
+  fd = open(SYSFS_GPIO_DIR "/unexport", O_WRONLY);
+  if (fd < 0) {
+    if (!silence_errors)
+      perror("gpio/unexport");
+    return fd;
+  }
+  // printf("gpio_unexport(%u)\n", gpio);
 
-	len = snprintf(buf, sizeof(buf), "%d", gpio);
-	err = write(fd, buf, len);
-	close(fd);
-	return err < 0 ? err : 0;
+  len = snprintf(buf, sizeof(buf), "%d", gpio);
+  err = write(fd, buf, len);
+  close(fd);
+  return err < 0 ? err : 0;
 }
 
 /****************************************************************
@@ -70,30 +70,29 @@ int gpio_unexport(unsigned int gpio)
  ****************************************************************/
 int gpio_set_dir(unsigned int gpio, unsigned int out_flag)
 {
-	int fd, err;
-	char buf[MAX_BUF];
+  int fd, err;
+  char buf[MAX_BUF];
 
-	snprintf(buf, sizeof(buf), SYSFS_GPIO_DIR "/gpio%d/direction", gpio);
+  snprintf(buf, sizeof(buf), SYSFS_GPIO_DIR "/gpio%d/direction", gpio);
 
-	fd = open(buf, O_WRONLY);
-	if (fd < 0)
-	{
-		perror("gpio/direction");
-		printf("--buf:'%s'\n", buf);
-		//exit(44);
-		return fd;
-	}
+  fd = open(buf, O_WRONLY);
+  if (fd < 0) {
+    perror("gpio/direction");
+    printf("--buf:'%s'\n", buf);
+    // exit(44);
+    return fd;
+  }
 #ifdef VERBOSE
-	printf("gpio_set_dir(%u, %s)\n", gpio, out_flag ? "out" : "in");
+  printf("gpio_set_dir(%u, %s)\n", gpio, out_flag ? "out" : "in");
 #endif
 
-	if (out_flag)
-		err = write(fd, "out", 4);
-	else
-		err = write(fd, "in", 3);
+  if (out_flag)
+    err = write(fd, "out", 4);
+  else
+    err = write(fd, "in", 3);
 
-	close(fd);
-	return err < 0 ? err : 0;
+  close(fd);
+  return err < 0 ? err : 0;
 }
 
 /****************************************************************
@@ -101,30 +100,29 @@ int gpio_set_dir(unsigned int gpio, unsigned int out_flag)
  ****************************************************************/
 int gpio_set_value(unsigned int gpio, unsigned int value)
 {
-	int fd, err;
-	char buf[MAX_BUF];
+  int fd, err;
+  char buf[MAX_BUF];
 
-	snprintf(buf, sizeof(buf), SYSFS_GPIO_DIR "/gpio%d/value", gpio);
+  snprintf(buf, sizeof(buf), SYSFS_GPIO_DIR "/gpio%d/value", gpio);
 
-	fd = open(buf, O_WRONLY);
-	if (fd < 0)
-	{
-		perror("gpio/set-value");
-		printf("--buf:'%s'\n", buf);
-		//exit(45);
-		return fd;
-	}
+  fd = open(buf, O_WRONLY);
+  if (fd < 0) {
+    perror("gpio/set-value");
+    printf("--buf:'%s'\n", buf);
+    // exit(45);
+    return fd;
+  }
 #ifdef VERBOSE
-	printf("gpio_set-value(%u, %u)\n", gpio, value);
+  printf("gpio_set-value(%u, %u)\n", gpio, value);
 #endif
 
-	if (value)
-		err = write(fd, "1", 2);
-	else
-		err = write(fd, "0", 2);
+  if (value)
+    err = write(fd, "1", 2);
+  else
+    err = write(fd, "0", 2);
 
-	close(fd);
-	return err < 0 ? err : 0;
+  close(fd);
+  return err < 0 ? err : 0;
 }
 
 /****************************************************************
@@ -132,39 +130,35 @@ int gpio_set_value(unsigned int gpio, unsigned int value)
  ****************************************************************/
 int gpio_get_value(unsigned int gpio, unsigned int *value)
 {
-	int fd, err;
-	char buf[MAX_BUF];
-	char ch;
+  int fd, err;
+  char buf[MAX_BUF];
+  char ch;
 
-	snprintf(buf, sizeof(buf), SYSFS_GPIO_DIR "/gpio%d/value", gpio);
+  snprintf(buf, sizeof(buf), SYSFS_GPIO_DIR "/gpio%d/value", gpio);
 
-	fd = open(buf, O_RDONLY);
-	if (fd < 0)
-	{
-		perror("gpio/get-value");
-		printf("--buf:'%s'\n", buf);
-		//exit(46);
-		return fd;
-	}
+  fd = open(buf, O_RDONLY);
+  if (fd < 0) {
+    perror("gpio/get-value");
+    printf("--buf:'%s'\n", buf);
+    // exit(46);
+    return fd;
+  }
 
-	err = read(fd, &ch, 1);
-	if (err > 0)
-	{
-		if (ch != '0')
-		{
-			*value = 1;
-		}
-		else
-		{
-			*value = 0;
-		}
-	}
+  err = read(fd, &ch, 1);
+  if (err > 0) {
+    if (ch != '0') {
+      *value = 1;
+    }
+    else {
+      *value = 0;
+    }
+  }
 #ifdef VERBOSE
-	printf("gpio_get-value(%u, ->%u)\n", gpio, *value);
+  printf("gpio_get-value(%u, ->%u)\n", gpio, *value);
 #endif
 
-	close(fd);
-	return 0;
+  close(fd);
+  return 0;
 }
 
 /****************************************************************
@@ -173,22 +167,21 @@ int gpio_get_value(unsigned int gpio, unsigned int *value)
 
 int gpio_set_edge(unsigned int gpio, char *edge)
 {
-	int fd, err;
-	char buf[MAX_BUF];
+  int fd, err;
+  char buf[MAX_BUF];
 
-	snprintf(buf, sizeof(buf), SYSFS_GPIO_DIR "/gpio%d/edge", gpio);
+  snprintf(buf, sizeof(buf), SYSFS_GPIO_DIR "/gpio%d/edge", gpio);
 
-	fd = open(buf, O_WRONLY);
-	if (fd < 0)
-	{
-		perror("gpio/set-edge");
-		printf("--buf:'%s'\n", buf);
-		return fd;
-	}
+  fd = open(buf, O_WRONLY);
+  if (fd < 0) {
+    perror("gpio/set-edge");
+    printf("--buf:'%s'\n", buf);
+    return fd;
+  }
 
-	err = write(fd, edge, strlen(edge) + 1);
-	close(fd);
-	return err < 0 ? err : 0;
+  err = write(fd, edge, strlen(edge) + 1);
+  close(fd);
+  return err < 0 ? err : 0;
 }
 
 /****************************************************************
@@ -197,73 +190,66 @@ int gpio_set_edge(unsigned int gpio, char *edge)
 
 int gpio_fd_open(unsigned int gpio)
 {
-	int fd;
-	char buf[MAX_BUF];
+  int fd;
+  char buf[MAX_BUF];
 
-	snprintf(buf, sizeof(buf), SYSFS_GPIO_DIR "/gpio%d/value", gpio);
+  snprintf(buf, sizeof(buf), SYSFS_GPIO_DIR "/gpio%d/value", gpio);
 
-	fd = open(buf, O_RDONLY | O_NONBLOCK);
-	if (fd < 0)
-	{
-		perror("gpio/fd_open");
-		printf("--buf:'%s'\n", buf);
-	}
-	return fd;
+  fd = open(buf, O_RDONLY | O_NONBLOCK);
+  if (fd < 0) {
+    perror("gpio/fd_open");
+    printf("--buf:'%s'\n", buf);
+  }
+  return fd;
 }
 
 /****************************************************************
  * gpio_fd_close
  ****************************************************************/
 
-int gpio_fd_close(int fd)
-{
-	return close(fd);
-}
+int gpio_fd_close(int fd) { return close(fd); }
 
 void ensure_export(unsigned int gpio)
 {
   int r = 9, am = 1600;
-  while (gpio_export(gpio) && r)
-  {
+
+  silence_errors = true;
+  while (gpio_export(gpio) && r) {
     --r;
     usleep(am);
     am *= 2;
   }
-  if (!r)
-  {
+  if (!r) {
     perror("ensure_export");
-    //exit(44);
+    // exit(44);
   }
+  silence_errors = false;
 }
 
 void ensure_set_dir(unsigned int gpio, unsigned int out_flag)
 {
   int r = 9, am = 1600;
-  while (gpio_set_dir(gpio, out_flag) && r)
-  {
+  while (gpio_set_dir(gpio, out_flag) && r) {
     --r;
     usleep(am);
     am *= 2;
   }
-  if (!r)
-  {
+  if (!r) {
     perror("ensure_set_dir");
-    //exit(45);
+    // exit(45);
   }
 }
 
 void ensure_set_value(unsigned int gpio, unsigned int value)
 {
   int r = 10, am = 1600;
-  while (gpio_set_value(gpio, value) && r)
-  {
+  while (gpio_set_value(gpio, value) && r) {
     --r;
     usleep(am);
     am *= 2;
   }
-  if (!r)
-  {
+  if (!r) {
     perror("ensure_set_value");
-    //exit(46);
+    // exit(46);
   }
 }
