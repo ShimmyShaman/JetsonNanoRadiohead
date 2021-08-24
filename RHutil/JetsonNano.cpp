4b58b4171520ea82809446589cd69208be8419b3
Adam Rasburn <BlackForestCheeseCake@protonmail.ch>
2021-08-24 14:03:55 +1200

    move to using JetsonGPIO instead of haphazard sysfs i/o code

--- RHutil/JetsonNano.cpp
+++ RHutil/JetsonNano.cpp
@@ -1,34 +1,31 @@
-// RasPi.cpp
+// JetsonNano.cpp
 //
 // Routines for implementing RadioHead on Raspberry Pi
 // using BCM2835 library for GPIO
 //
 // Contributed by Mike Poublon and used with permission
 
-
 #include <RadioHead.h>
-
 
 #if (RH_PLATFORM == RH_PLATFORM_JETSON_NANO)
 #include <sys/time.h>
 #include <time.h>
 #include <unistd.h>
 
-#include "JetsonNano.h"
-#include "JetsonNano_gpio.h"
+#include <JetsonGPIO.h>
 
-//Initialize the values for sanity
+// Initialize the values for sanity
 timeval RHStartTime;
 
 void SPIClass::begin()
 {
-    puts ("TODO 5524");
-  //Set SPI Defaults
-//   uint16_t divider = BCM2835_SPI_CLOCK_DIVIDER_256;
-//   uint8_t bitorder = BCM2835_SPI_BIT_ORDER_MSBFIRST;
-//   uint8_t datamode = BCM2835_SPI_MODE0;
+  puts("TODO 5524");
+  // Set SPI Defaults
+  //   uint16_t divider = BCM2835_SPI_CLOCK_DIVIDER_256;
+  //   uint8_t bitorder = BCM2835_SPI_BIT_ORDER_MSBFIRST;
+  //   uint8_t datamode = BCM2835_SPI_MODE0;
 
-//   begin(divider, bitorder, datamode);
+  //   begin(divider, bitorder, datamode);
 }
 
 void SPIClass::begin(uint16_t divider, uint8_t bitOrder, uint8_t dataMode)
@@ -37,81 +34,88 @@ void SPIClass::begin(uint16_t divider, uint8_t bitOrder, uint8_t dataMode)
   setBitOrder(bitOrder);
   setDataMode(dataMode);
 
-  //Set CS pins polarity to low
+  // Set CS pins polarity to low
   puts("TODO 8857");
-//   bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, 0);
+  //   bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, 0);
 
-//   bcm2835_spi_begin();
+  //   bcm2835_spi_begin();
 
-  //Initialize a timestamp for millis calculation
+  // Initialize a timestamp for millis calculation
   gettimeofday(&RHStartTime, NULL);
 }
 
 void SPIClass::end()
 {
   puts("TODO 24242");
-  //End the SPI
-//   bcm2835_spi_end();
+  // End the SPI
+  //   bcm2835_spi_end();
 }
 
 void SPIClass::setBitOrder(uint8_t bitOrder)
 {
   puts("TODO 4141");
-//   //Set the SPI bit Order
-//   bcm2835_spi_setBitOrder(bitOrder);
+  //   //Set the SPI bit Order
+  //   bcm2835_spi_setBitOrder(bitOrder);
 }
 
 void SPIClass::setDataMode(uint8_t mode)
 {
   puts("TODO 14141");
-//   //Set SPI data mode
-//   bcm2835_spi_setDataMode(mode);
+  //   //Set SPI data mode
+  //   bcm2835_spi_setDataMode(mode);
 }
 
 void SPIClass::setClockDivider(uint16_t rate)
 {
   puts("TODO 65652");
-//   //Set SPI clock divider
-//   bcm2835_spi_setClockDivider(rate);
+  //   //Set SPI clock divider
+  //   bcm2835_spi_setClockDivider(rate);
 }
 
 byte SPIClass::transfer(byte _data)
 {
   puts("TODO 5522");
-//   Set which CS pin to use for next transfers
-//   bcm2835_spi_chipSelect(BCM2835_SPI_CS0);
-//   Transfer 1 byte
+  //   Set which CS pin to use for next transfers
+  //   bcm2835_spi_chipSelect(BCM2835_SPI_CS0);
+  //   Transfer 1 byte
   byte data = 0x11;
-//   data = bcm2835_spi_transfer((uint8_t)_data);
+  //   data = bcm2835_spi_transfer((uint8_t)_data);
   return data;
 }
 
 void pinMode(unsigned char pin, unsigned char mode)
 {
-    gpio_set_dir(pin, mode);
+  GPIO::Directions direction;
+  switch (mode) {
+    case OUTPUT:
+      direction = GPIO::Directions::OUT;
+      break;
+    default:
+      fprintf(stderr, "[ERROR] unhandled mode: %u\n", mode);
+      return;
+  }
+
+  GPIO::setup(pin, direction);
 }
 
-void digitalWrite(unsigned char pin, unsigned char value)
-{
-    gpio_set_value(pin, value);
-}
+void digitalWrite(unsigned char pin, unsigned char value) { GPIO::output(pin, value); }
 
 unsigned long millis()
 {
-  //Declare a variable to store current time
+  // Declare a variable to store current time
   struct timeval RHCurrentTime;
-  //Get current time
-  gettimeofday(&RHCurrentTime,NULL);
-  //Calculate the difference between our start time and the end time
+  // Get current time
+  gettimeofday(&RHCurrentTime, NULL);
+  // Calculate the difference between our start time and the end time
   unsigned long difference = ((RHCurrentTime.tv_sec - RHStartTime.tv_sec) * 1000);
-  difference += ((RHCurrentTime.tv_usec - RHStartTime.tv_usec)/1000);
-  //Return the calculated value
+  difference += ((RHCurrentTime.tv_usec - RHStartTime.tv_usec) / 1000);
+  // Return the calculated value
   return difference;
 }
 
-void delay (unsigned long ms)
+void delay(unsigned long ms)
 {
-  //Implement Delay function
+  // Implement Delay function
   // struct timespec ts;
   // ts.tv_sec=0;
   // ts.tv_nsec=(ms * 1000);
@@ -129,14 +133,14 @@ long random(long min, long max)
 
 void SerialSimulator::begin(int baud)
 {
-  //No implementation neccesary - Serial emulation on Linux = standard console
+  // No implementation neccesary - Serial emulation on Linux = standard console
   //
-  //Initialize a timestamp for millis calculation - we do this here as well in case SPI
-  //isn't used for some reason
+  // Initialize a timestamp for millis calculation - we do this here as well in case SPI
+  // isn't used for some reason
   gettimeofday(&RHStartTime, NULL);
 }
 
-size_t SerialSimulator::println(const char* s)
+size_t SerialSimulator::println(const char *s)
 {
   print(s);
   printf("\n");
@@ -144,7 +148,7 @@ size_t SerialSimulator::println(const char* s)
   return 0;
 }
 
-size_t SerialSimulator::print(const char* s)
+size_t SerialSimulator::print(const char *s)
 {
   printf("%s", s);
 
--- RHutil/JetsonNano_gpio.h
+++ RHutil/JetsonNano_gpio.h
@@ -1,19 +1,19 @@
-#ifndef JETSON_NANO_GPIO_H
-#define JETSON_NANO_GPIO_H
+// #ifndef JETSON_NANO_GPIO_H
+// #define JETSON_NANO_GPIO_H
 
-// #define RH_HAVE_HARDWARE_SPI
+// // #define RH_HAVE_HARDWARE_SPI
 
-int gpio_export(unsigned int gpio);
-int gpio_unexport(unsigned int gpio);
-int gpio_set_dir(unsigned int gpio, unsigned int out_flag);
-int gpio_set_value(unsigned int gpio, unsigned int value);
-int gpio_get_value(unsigned int gpio, unsigned int *value);
-int gpio_set_edge(unsigned int gpio, char *edge);
-int gpio_fd_open(unsigned int gpio);
-int gpio_fd_close(int fd);
+// int gpio_export(unsigned int gpio);
+// int gpio_unexport(unsigned int gpio);
+// int gpio_set_dir(unsigned int gpio, unsigned int out_flag);
+// int gpio_set_value(unsigned int gpio, unsigned int value);
+// int gpio_get_value(unsigned int gpio, unsigned int *value);
+// int gpio_set_edge(unsigned int gpio, char *edge);
+// int gpio_fd_open(unsigned int gpio);
+// int gpio_fd_close(int fd);
 
-void ensure_export(unsigned int gpio);
-void ensure_set_dir(unsigned int gpio, unsigned int out_flag);
-void ensure_set_value(unsigned int gpio, unsigned int value);
+// void ensure_export(unsigned int gpio);
+// void ensure_set_dir(unsigned int gpio, unsigned int out_flag);
+// void ensure_set_value(unsigned int gpio, unsigned int value);
 
-#endif // JETSON_NANO_GPIO_H
+// #endif // JETSON_NANO_GPIO_H
